b687f0ea3c2b81462a4b48e86da5c404fee9722c
nampro503 <daonam1497@gmail.com>
2021-12-04 21:54:22 +0700

    Bai Tap ngay 14/4/2021

---
+++ Baitap 1/timthu.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+		printf ("/Nhap vao 1 so nguyen :");
+		scanf ("%d", &n);
+
+	if (2<=n && n<= 7)	{
+		printf ("/Day la thu %d",n);
+		
+	}
+	else {
+		if(n=8){
+			printf("Chu Nhat");
+			
+		}
+		else{
+			printf ("Khong Phai Ngay Trong Tuan");
+		}
+	}
+	
+	
+}
---
+++ Baitap 2/bai 2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int n=4000;
+	n=n+n*0.08;
+	n=n+n*0.08;
+	n=n+n*0.08;
+	n=n+n*0.08;
+	printf ("%d", n);
+}
