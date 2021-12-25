f3f6c980bac78a2a945ce8d88b962485361d2936
nampro503 <daonam1497@gmail.com>
2021-12-25 14:40:50 +0700

    Update Untitled3.cpp

--- Assignment9/bai 5 6/Untitled3.cpp
+++ Assignment9/bai 5 6/Untitled3.cpp
@@ -1,4 +1,30 @@
 #include <stdio.h>
+#include <stdio.h>
+int chuvi(int a,int b, int c);
+double dientich(int a,int b,int c);
+
+int chuvi(int a,int b, int c){
+	return a+b+c;
+}
+double dientich(int a,int b,int c){
+	double p=chuvi(a,b,c)/2.0;
+	double dt=sqrt(p*(p-a)*(p-b)*(p-c));
+	return dt;
+	
+}
+
+int tinhTong(int x){
+	int kq=0,t;
+	
+		
+    while(x>0){
+    	t=x%10;
+    	kq=kq+t;
+    	x/=10;
+	}
+	return kq;
+}
+
 int UCLN(int a,int b){
 	if(a<0|| b<0){
 		return 1;
@@ -36,6 +62,10 @@ int main(){
 	scanf("%d",&a);
 	printf("Nhap vao b:");
 	scanf("%d",&b);
+	int n;
+	printf("Nhap n=");
+	scanf("%d",&n);
+	printf("Tong cac chu=%d",tinhTong(n));
 
 }
 
